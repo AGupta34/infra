96d33e54c55c8bdfbc7fccd68d3b212ceecb7098
arungupta09 <arun.gupta1@telus.com>
2022-12-08 22:29:32 +0530

    modified files

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,3 +23,15 @@ resource "google_artifact_registry_repository" "docker-builds" {
   description = "Docker Builds"
   format = "DOCKER"
 }
+
+resource "google_service_account" "cloud-run-sa" {
+  account_id   = "cloud-run-sa"
+  display_name = "Cloud Run Service Account"
+  project      = var.project
+}
+
+resource "google_project_iam_member" "sa-editor" {
+  project = var.project
+  role    = "roles/editor"
+  member  = "serviceAccount:${google_service_account.cloud-run-sa.email}"
+}
