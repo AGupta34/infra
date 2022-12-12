5f5c877880d06fc1b8dc7509028b9cda27250929
arungupta09 <arun.gupta1@telus.com>
2022-12-12 18:44:46 +0530

    database added

--- terraform/main.tf
+++ terraform/main.tf
@@ -117,7 +117,7 @@ resource "google_sql_database_instance" "todo_database" {
         disk_type             = "PD_SSD"
         ip_configuration {
             ipv4_enabled    = "false"
-            private_network = google_compute_network.vpc_network.name
+            private_network = "google_compute_network.vpc_network.name"
         }
         location_preference {
             zone = "northamerica-northeast1-a"
